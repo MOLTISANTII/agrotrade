b8514533e781ea00c3d13f0feeff086b90dcbefd
Charles Amanze <amanzeprince@gmail.com>
2023-11-25 20:10:43 +0100

    navbar mobile view design

--- components/navbar.js
+++ components/navbar.js
@@ -10,23 +10,36 @@ const montserrat_500 = Montserrat({
 export default function NavBar () {
     return (
         <div>
-            <nav className="h-[58px] flex items-center px-20">
-                {/*desktop navbar */}
-                <div className="w-full flex flex-row justify-between items-center">
-                    <ul className="flex flex-rol items-center gap-12">
+            <nav className="h-[58px] flex items-center px-4 sm:px-20 justify-between">
+                {/* mobile navbar */}
+                <div className="w-full flex flex-row justify-between items-center sm:hidden">
+                    <ul className="flex flex-row items-center gap-4">
                         <li>
                             <Image width={38} height={38} src='/AGROTRADE.png' alt="logo"/>
                         </li>
-                    <li className={`${montserrat_500.className} text-green-700 hover:text-yellow-700`}><Link href='/'>Home</Link></li>
-                    <li className={`${montserrat_500.className} text-green-700 hover:text-yellow-700`}><Link href='/about-us'>About Us</Link></li>
-                    <li className={`${montserrat_500.className} text-green-700 hover:text-yellow-700`}><Link href='/contact-us'>Contact Us</Link></li>
-                    <li className={`${montserrat_500.className} text-green-700 hover:text-yellow-700`}><Link href='/products'>Products</Link></li>
+                        <li className={`${montserrat_500.className} text-green-700 hover:text-yellow-700`}><Link href='/products'>Products</Link></li>
+                    
+                    </ul>
+                    <div>
+                        <Link href='/auth/signup'>Sign up</Link>
+                    </div>
+                </div>
+
+                {/* desktop navbar */}
+                <div className="hidden sm:flex flex-row items-center gap-12">
+                    <ul className="flex flex-row items-center gap-12">
+                        <li>
+                            <Image width={38} height={38} src='/AGROTRADE.png' alt="logo"/>
+                        </li>
+                        <li className={`${montserrat_500.className} text-green-700 hover:text-yellow-700`}><Link href='/'>Home</Link></li>
+                        <li className={`${montserrat_500.className} text-green-700 hover:text-yellow-700`}><Link href='/about-us'>About Us</Link></li>
+                        <li className={`${montserrat_500.className} text-green-700 hover:text-yellow-700`}><Link href='/contact-us'>Contact Us</Link></li>
+                        <li className={`${montserrat_500.className} text-green-700 hover:text-yellow-700`}><Link href='/products'>Products</Link></li>
                     </ul>
 
                     <Link href='/auth/signup'>Sign up</Link>
                 </div>
 
-                
             </nav>
 
             <div className="grid grid-cols-3">
